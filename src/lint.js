d9d647da53cc55331dee391be8a8ad76e3e67c55
Garth Poitras <garth22@gmail.com>
2018-06-27 10:15:47 -0400

    fix lint command

--- src/lint.js
+++ src/lint.js
@@ -1,7 +1,7 @@
 const run = require('./run')
 const configPath = require.resolve('./configs/.eslintrc.js')
 
-const command = 'eslint'
-const recommendedArgs = `DEBUG=eslint:cli-engine eslint . --config ${configPath} --ext .js,.ts,.graphql --ignore-path .gitignore \`[ -z "$CI" ] && echo '--cache --fix'\``
+const command = 'DEBUG=eslint:cli-engine eslint'
+const recommendedArgs = `. --config ${configPath} --ext .js,.ts,.graphql --ignore-path .gitignore \`[ -z "$CI" ] && echo '--cache --fix'\``
 
 module.exports = opts => run(command, opts, recommendedArgs)
