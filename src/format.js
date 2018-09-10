dd645d656b11468e6fe537000efaa42ab569c396
Garth Poitras <garth22@gmail.com>
2018-09-10 10:33:16 -0400

    Allow extending eslint config. 0.6.0

--- src/format.js
+++ src/format.js
@@ -2,6 +2,6 @@ const run = require('./run')
 const configPath = require.resolve('./configs/.prettierrc.js')
 
 const command = 'prettier'
-const recommendedArgs = `'**/*.{js,ts,graphql,css,md}' --config ${configPath} --ignore-path .gitignore \`[ -z "$CI" ] && echo '--write' || echo '--list-different'\``
+const recommendedArgs = `'**/*.{js,ts,graphql,css,md,json}' --config ${configPath} --ignore-path .gitignore \`[ -z "$CI" ] && echo '--write' || echo '--list-different'\``
 
 module.exports = opts => run(command, opts, recommendedArgs)
--- src/lint.js
+++ src/lint.js
@@ -1,7 +1,17 @@
+const { join } = require('path')
+const { existsSync } = require('fs')
 const run = require('./run')
 const configPath = require.resolve('./configs/.eslintrc.js')
 
+const pwd = process.env.PWD
+const hasOwnConfig =
+  existsSync(join(pwd, '.eslintrc.js')) ||
+  existsSync(join(pwd, '.eslintrc')) ||
+  Boolean(require(join(pwd, 'package.json')).eslintConfig)
+
 const command = 'DEBUG=eslint:cli-engine eslint'
-const recommendedArgs = `. --config ${configPath} --ext .js,.ts,.graphql --ignore-path .gitignore \`[ -z "$CI" ] && echo '--cache --fix'\``
+const recommendedArgs = `. --ignore-path .gitignore \`[ -z "$CI" ] && echo '--cache --fix'\` ${
+  hasOwnConfig ? '' : '--config ' + configPath
+}`
 
 module.exports = opts => run(command, opts, recommendedArgs)
