a76621c54729764c6e15c2d0652bfe10c878930e
gpoitch <garth22@gmail.com>
2018-12-28 23:16:11 -0500

    Upgrade deps. Improved eslint parser + ts. v0.9.0

--- src/configs/.eslintrc.js
+++ src/configs/.eslintrc.js
@@ -1,36 +1,33 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    mocha: true
+  parser: 'typescript-eslint-parser',
+  parserOptions: { jsx: true },
+  env: { node: true, browser: true },
+  globals: {
+    // mocha (sans 'context')
+    describe: true,
+    it: true,
+    before: true,
+    after: true
   },
-  parser: 'babel-eslint',
   extends: ['plugin:react/recommended', 'standard', 'prettier'],
-  plugins: ['css-modules'],
-  settings: { react: { version: '16.0' } },
+  plugins: ['typescript', 'css-modules'],
   rules: {
     'dot-notation': 'error',
     'no-console': 'error',
     'no-param-reassign': 'error',
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+    'no-var': 'error',
     'object-shorthand': 'error',
     'prefer-const': 'error',
     'prefer-object-spread': 'error',
+    'typescript/no-unused-vars': 'error',
+    'css-modules/no-undef-class': 'error',
     'react/prefer-stateless-function': 'error',
     'react/no-array-index-key': 'error',
     'react/no-typos': 'error',
     'react/no-unknown-property': ['error', { ignore: ['class', 'for'] }],
-    'react/no-deprecated': 'warn',
     'react/no-unescaped-entities': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off'
-  },
-  overrides: {
-    files: ['**.ts'],
-    parser: 'typescript-eslint-parser',
-    plugins: ['typescript'],
-    rules: {
-      'typescript/no-unused-vars': 'error'
-    }
   }
 }
