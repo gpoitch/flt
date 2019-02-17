bbd1ff2e3006aee849b7ccafa9ed9de7765f4fd7
gpoitch <garth22@gmail.com>
2019-02-17 10:21:44 -0500

    upgrade deps. 0.10.0

--- src/configs/.eslintrc.js
+++ src/configs/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: 'typescript-eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: { jsx: true },
   env: { node: true, browser: true },
   globals: {
@@ -9,8 +9,17 @@ module.exports = {
     before: true,
     after: true
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
-  plugins: ['typescript', 'css-modules'],
+  extends: [
+    'plugin:import/errors',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'prettier/react',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['@typescript-eslint', 'css-modules'],
   rules: {
     'dot-notation': 'error',
     'no-console': 'error',
@@ -20,7 +29,6 @@ module.exports = {
     'object-shorthand': 'error',
     'prefer-const': 'error',
     'prefer-object-spread': 'error',
-    'typescript/no-unused-vars': 'error',
     'css-modules/no-undef-class': 'error',
     'react/prefer-stateless-function': 'error',
     'react/no-array-index-key': 'error',
@@ -28,6 +36,7 @@ module.exports = {
     'react/no-unknown-property': ['error', { ignore: ['class', 'for'] }],
     'react/no-unescaped-entities': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 }
