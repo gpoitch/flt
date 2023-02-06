8daf434166081f783993679c69e46fa5492d614d
Garth Poitras <411908+gpoitch@users.noreply.github.com>
2023-02-06 08:16:21 -0500

    add cjs check

--- src/has-own-config.js
+++ src/has-own-config.js
@@ -6,6 +6,7 @@ const pwd = process.cwd()
 module.exports = function hasOwnConfig(moduleName, packageField) {
   return (
     existsSync(join(pwd, `.${moduleName}rc.js`)) ||
+    existsSync(join(pwd, `.${moduleName}rc.cjs`)) ||
     existsSync(join(pwd, `.${moduleName}rc`)) ||
     existsSync(join(pwd, `${moduleName}.config.js`)) ||
     Boolean(require(join(pwd, 'package.json'))[packageField || moduleName])
